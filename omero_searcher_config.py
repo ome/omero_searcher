05de4c65838ddc80eae9ea2d7c0c1b0da80afd6f
Simon Li <spli@dundee.ac.uk>
2013-04-24 22:29:16 +0100

    Changed default omero_contentdb_path to /OMERO/pyslid.data

--- omero_searcher_config.py
+++ omero_searcher_config.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 # Path to the directory for storing ContentDB files
-import os
-omero_contentdb_path = os.path.join(os.environ['HOME'], 'var', 'pyslid.data')
+omero_contentdb_path = '/OMERO/pyslid.data'
 
 
