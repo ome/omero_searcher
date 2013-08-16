9f1892a8fe10b49b732b77b71521667ae5af4d61
Simon Li <spli@dundee.ac.uk>
2013-08-16 11:13:39 +0100

    Changed VM install script to work with the new image

--- docs/vm/setup_omero_searcher.sh
+++ docs/vm/setup_omero_searcher.sh
@@ -29,34 +29,39 @@ DL_FOLDER=OMERO-searcher
 unzip -o $DL_ARCHIVE
 cd $DL_FOLDER
 
-echo $PASSWORD | sudo -S apt-get install -qy libfreeimage3 python-pip
+#echo $PASSWORD | sudo -S apt-get install -qy libfreeimage3 python-pip
 # The version of pip on the VM image doesn't install recursive dependencies
 # from pyslid setup.py
-echo $PASSWORD | sudo -S pip install mahotas==0.9.4
-echo $PASSWORD | sudo -S pip install milk==0.4.3
-echo $PASSWORD | sudo -S pip install pymorph==0.96
-echo $PASSWORD | sudo -S pip install pyslic==0.6.1
+#echo $PASSWORD | sudo -S pip install mahotas==0.9.4
+#echo $PASSWORD | sudo -S pip install milk==0.4.3
+#echo $PASSWORD | sudo -S pip install pymorph==0.96
+#echo $PASSWORD | sudo -S pip install pyslic==0.6.1
+echo $PASSWORD | sudo -S apt-get install -qy libfreeimage3
+
 
 # Need to wrap pip with sudo
 # The version of pip is useless, and ignores git branch specifications, so
 # disable parsing of the requirements file and install everything manually
 
-pushd pyslid
-git remote add update https://github.com/manics/pyslid.git
-git fetch update
-git checkout update/merge_all
-echo $PASSWORD | sudo -S pip install $PWD
-popd
+#pushd pyslid
+#git remote add update https://github.com/manics/pyslid.git
+#git fetch update
+#git checkout update/merge_all
+#echo $PASSWORD | sudo -S pip install $PWD
+#popd
 
-echo $PASSWORD | sudo -S pip install git+git://github.com/icaoberg/ricerca.git@master
+#echo $PASSWORD | sudo -S pip install git+git://github.com/icaoberg/ricerca.git@master
 
 
-#sed -i.bak -e "s/^pip /echo $PASSWORD | sudo -S pip /" install.sh
-sed -i.bak -e "s/^pip /#pip /" install.sh
+sed -i.bak -e "s/^pip /echo $PASSWORD | sudo -S pip /" install.sh
+#sed -i.bak -e "s/^pip /#pip /" install.sh
 ./install.sh $OMERO_PATH
 
 mkdir -p "$PYSLID_DATA_DIR"
 sed -i.bak -e \
     "s%^omero_contentdb_path = .*$%omero_contentdb_path = \\'$PYSLID_DATA_DIR\\'%" \
     "$OMERO_PATH/lib/python/omeroweb/omero_searcher/omero_searcher_config.py"
+
+echo $PASSWORD | sudo -S /etc/init.d/omero restart
+echo $PASSWORD | sudo -S /etc/init.d/omero-web restart
 
