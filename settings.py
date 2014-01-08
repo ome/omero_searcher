9bfe6d03b91cb9f204e8fe4678aa6c2450c96af6
Simon Li <spli@dundee.ac.uk>
2014-01-08 16:39:35 +0000

    Explicitly check the django version

--- settings.py
+++ settings.py
@@ -5,13 +5,12 @@ from django.conf import settings
 
 # We can directly manipulate the settings
 # E.g. add plugins to RIGHT_PLUGINS list
-try:
-    # OMERO 4.4
+import django
+if django.VERSION < (1, 6):
     settings.RIGHT_PLUGINS.append([
             "Searcher",
             "searcher/plugin_config/right_search_form.js.html",
             "right_search_form"])
-except AttributeError:
-    # OMERO 5: Need to manually configure:
-    # omero config set omero.ui.right_plugins '[[...], ... ["Searcher", "searcher/plugin_config/right_search_form.js.html", "right_search_form"]]'
-    pass
+
+# OMERO 5: Need to manually configure:
+# omero config set omero.ui.right_plugins '[[...], ... ["Searcher", "searcher/plugin_config/right_search_form.js.html", "right_search_form"]]'
