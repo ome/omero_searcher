15177c9e2ba71a06953a9b7a0af7d440dae71a57
Simon Li <spli@dundee.ac.uk>
2014-01-09 10:25:52 +0000

    Revert "Add --nodeps option to install script so we can manually choose deps"
    This reverts commit 674cf9c201f939f3a04075b312dd16a06005daa2.
    
    I'm moving this commit into a separate PR to avoid conflicts with later work

--- install.sh
+++ install.sh
@@ -41,9 +41,8 @@ check that it is running.
 EOF
 
 usage() {
-    echo "USAGE: `basename $0` OMERO_PREFIX [--nodeps]"
+    echo "USAGE: `basename $0` OMERO_PREFIX"
     echo "  OMERO_PREFIX: The root directory of the OMERO server installation"
-    echo "  --nodeps: Don't install requirements"
     exit $1
 }
 
@@ -63,13 +62,9 @@ if [ $# -eq 0 -o "$1" = "-h" ]; then
     usage 0
 fi
 
-NODEPS=0
 if [ $# -ne 1 ]; then
-    if [ $# -gt 2 -o "$2" != "--nodeps" ]; then
-        echo "Unexpected arguments"
-        usage 2
-    fi
-    NODEPS=1
+    echo "Unexpected arguments"
+    usage 2
 fi
 
 if [ ! -d "$1" ]; then
@@ -83,21 +78,17 @@ SCRIPT_DEST="$OMERO_SERVER/lib/scripts/searcher"
 WEB_DEST="$OMERO_SERVER/lib/python/omeroweb/omero_searcher"
 CONFIG="$WEB_DEST/omero_searcher_config.py"
 
-if [ $NODEPS -eq 1 ]; then
-    echo "Skipping dependencies"
-else
-    echo "Checking for PIL, numpy and scipy"
-    check_py_mod PIL "ERROR: Please install PIL" 1
-    check_py_mod numpy "ERROR: Please install numpy" 1
-    check_py_mod scipy "ERROR: Please install scipy" 1
-    check_py_mod tables \
+echo "Checking for PIL, numpy and scipy"
+check_py_mod PIL "ERROR: Please install PIL" 1
+check_py_mod numpy "ERROR: Please install numpy" 1
+check_py_mod scipy "ERROR: Please install scipy" 1
+check_py_mod tables \
 "WARNING: Pytables appears to be missing. If OMERO.tables is running
 on a different server this doesn't matter, otherwise please check
 OMERO.tables is running." 0
 
-    echo "Installing python dependencies"
-    pip install -r requirements.txt
-fi
+echo "Installing python dependencies"
+pip install -r requirements.txt
 
 if [ -e "$CONFIG" ]; then
     echo "Saving old configuration"
